340666d285802f122f54146bc39455cd7c1cf774
Girish Rajani <girish.r.rajani@gmail.com>
2021-02-07 00:34:17 +0530

    AppBar Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(
+    MaterialApp(
+      home: PassWordGen(),
+    ),
+  );
+}
+
+class PassWordGen extends StatefulWidget {
+  @override
+  _PassWordGenState createState() => _PassWordGenState();
+}
+
+class _PassWordGenState extends State<PassWordGen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        title: Text(
+          'Password Generator',
+        ),
+      ),
+    );
+  }
+}
