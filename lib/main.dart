0b8f8624f21bfad80dde696d7bf51f1afb2ecbee
Girish Rajani <girish.r.rajani@gmail.com>
2021-02-08 01:36:28 +0530

    Checkbox List Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,6 +15,10 @@ class PassWordGen extends StatefulWidget {
 }
 
 class _PassWordGenState extends State<PassWordGen> {
+  var _char = false;
+  var _num = false;
+  var _lower = false;
+  var _upper = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +28,61 @@ class _PassWordGenState extends State<PassWordGen> {
         title: Text(
           'Password Generator',
         ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          CheckboxListTile(
+            value: _lower,
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text('Lowercase'),
+            secondary: Icon(CupertinoIcons.info),
+            onChanged: (bool value) {
+              setState(() {
+                _lower = value;
+              });
+            },
+            activeColor: Colors.redAccent,
+          ),
+          CheckboxListTile(
+            value: _upper,
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text('UpperCase'),
+            secondary: Icon(CupertinoIcons.italic),
+            onChanged: (bool value) {
+              setState(() {
+                _upper = value;
+              });
+            },
+            activeColor: Colors.redAccent,
+          ),
+          CheckboxListTile(
+            title: Text('Special Characters'),
+            secondary: Icon(CupertinoIcons.money_dollar),
+            value: _char,
+            controlAffinity: ListTileControlAffinity.leading,
+            onChanged: (bool value) {
+              setState(() {
+                _char = value;
+              });
+            },
+            activeColor: Colors.redAccent,
+          ),
+          CheckboxListTile(
+            value: _num,
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text('Numbers'),
+            secondary: Icon(CupertinoIcons.number_circle),
+            onChanged: (bool value) {
+              setState(() {
+                _num = value;
+              });
+            },
+            activeColor: Colors.redAccent,
+          ),
+        ],
       ),
     );
   }
